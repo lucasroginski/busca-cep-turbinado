ad041dfc5ce1eedd407d7010fd5c30728c29af78
Your Name <you@example.com>
2025-04-16 08:42:19 -0300

    Primeiro commit

---
+++ src/script.js
@@ -0,0 +1,24 @@
+function pesquisarCep() {
+    var cep = document.getElementById("cep").value.replace(/\D/g, '');
+    var resultado = document.getElementById("resultado");
+    if (!cep || cep.length != 8) {
+        resultado.innerHTML = '<p style="color: red">CEP inválido! digite 8 números</p>';
+        return;
+
+    }
+    fetch(`https://viacep.com.br/ws/${cep}/json`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                resultado.innerHTML = '<p style="color: red">CEP não encontrado!</p>';
+                return;
+            }else{
+                resultado.innerHTML=`<p><strong>Logradouro: </strong>${data.logradouro}</p>
+                <p><strong>Bairro: </strong>${data.bairro}</p>
+                <p><strong>Cidade: </strong>${data.localidade}</p>
+                <p><strong>Estado: </strong>${data.uf}</p>`;
+           }
+
+
+        })
+}
